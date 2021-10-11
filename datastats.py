4055b33eb3ee7587bf53e101ca8988620e36168a
Christian Dreier <flipper.x3r@googlemail.com>
2021-10-11 13:13:53 +0200

    Improve identifiers

--- datastats.py
+++ datastats.py
@@ -12,29 +12,26 @@ EMPLOYEE_JSON = """
 """
 
 
-def stats(data, iage, isalary):
-    # iage and isalary are the starting age and salary used to
-    # compute the average yearly increase of salary.
+def stats(employees_data, starting_age, starting_salary):
+    # starting_age and starting_salary are used to compute the average yearly increase of salary.
 
     # Compute average yearly increase
-    average_age_increase = math.floor(sum([e['age'] for e in data]) / len(data)) - iage
-    average_salary_increase = math.floor(sum([int(e['salary'][1:]) for e in data]) / len(data)) - isalary
-
+    average_age_increase = math.floor(sum([employee['age'] for employee in employees_data]) / len(employees_data)) - starting_age
+    average_salary_increase = math.floor(sum([int(employee['salary'][1:]) for employee in employees_data]) / len(employees_data)) - starting_salary
     yearly_avg_increase = math.floor(average_salary_increase / average_age_increase)
 
     # Compute max salary
-    salaries = [int(e['salary'][1:]) for e in data]
+    salaries = [int(employee['salary'][1:]) for employee in employees_data]
     threshold = '€' + str(max(salaries))
-
-    max_salary = [e for e in data if e['salary'] == threshold]
+    max_salary = [employee for employee in employees_data if employee['salary'] == threshold]
 
     # Compute min salary
-    salaries = [int(d['salary'][1:]) for d in data]
-    min_salary = [e for e in data if e['salary'] == '€{}'.format(str(min(salaries)))]
+    salaries = [int(employee['salary'][1:]) for employee in employees_data]
+    min_salary = [employee for employee in employees_data if employee['salary'] == '€{}'.format(str(min(salaries)))]
 
     return json.dumps({
-        'avg_age': math.floor(sum([e['age'] for e in data]) / len(data)),
-        'avg_salary': math.floor(sum([int(e['salary'][1:]) for e in data]) / len(data)),
+        'avg_age': math.floor(sum([employee['age'] for employee in employees_data]) / len(employees_data)),
+        'avg_salary': math.floor(sum([int(e['salary'][1:]) for e in employees_data]) / len(employees_data)),
         'avg_yearly_increase': yearly_avg_increase,
         'max_salary': max_salary,
         'min_salary': min_salary,
@@ -42,7 +39,7 @@ def stats(data, iage, isalary):
 
 
 def main():
-    print(stats(json.loads(f"[{EMPLOYEE_JSON}]"), iage=24, isalary=30000))
+    print(stats(json.loads(f"[{EMPLOYEE_JSON}]"), starting_age=24, starting_salary=30000))
 
 
 if __name__ == "__main__":
