2f203ccb2490c7593ebd22c6d3f74d71bf5a9137
Christian Dreier <flipper.x3r@googlemail.com>
2021-10-11 14:31:18 +0200

    Add abstraction between JSON and employee data

--- datastats.py
+++ datastats.py
@@ -12,34 +12,64 @@ EMPLOYEE_JSON = """
 """
 
 
+class Employee:
+    def __init__(self, surname, name, age, salary, currency):
+        self.surname = surname
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.currency = currency
+
+    @classmethod
+    def from_json(cls, data):
+        if isinstance(data, str):
+            data = json.loads(data)
+
+        if isinstance(data, dict):
+            return cls(
+                data["surname"],
+                data["name"],
+                data["age"],
+                int(data["salary"][1:]),
+                data["salary"][0])
+        elif isinstance(data, list):
+            return [cls.from_json(employee_data) for employee_data in data]
+        else:
+            raise ValueError("Given JSON could not be parsed")
+
+    def to_dict(self):
+        return {
+            "age": self.age,
+            "surname": self.surname,
+            "name": self.name,
+            "salary": f"{self.currency}{self.salary}",
+        }
+
+
 def stats(employees_data, starting_age, starting_salary):
     # starting_age and starting_salary are used to compute the average yearly increase of salary.
 
-    # Compute average yearly increase
-    average_age_increase = math.floor(sum([employee['age'] for employee in employees_data]) / len(employees_data)) - starting_age
-    average_salary_increase = math.floor(sum([int(employee['salary'][1:]) for employee in employees_data]) / len(employees_data)) - starting_salary
+    employees = Employee.from_json(employees_data)
+
+    average_age_increase = math.floor(sum([employee.age for employee in employees]) / len(employees)) - starting_age
+    average_salary_increase = math.floor(sum([employee.salary for employee in employees]) / len(employees)) - starting_salary
     yearly_avg_increase = math.floor(average_salary_increase / average_age_increase)
 
-    # Compute max salary
-    salaries = [int(employee['salary'][1:]) for employee in employees_data]
-    threshold = '€' + str(max(salaries))
-    max_salary = [employee for employee in employees_data if employee['salary'] == threshold]
-
-    # Compute min salary
-    salaries = [int(employee['salary'][1:]) for employee in employees_data]
-    min_salary = [employee for employee in employees_data if employee['salary'] == '€{}'.format(str(min(salaries)))]
+    salaries = [employee.salary for employee in employees]
+    max_salary_employees = [employee for employee in employees if employee.salary == max(salaries)]
+    min_salary_employees = [employee for employee in employees if employee.salary == min(salaries)]
 
     return json.dumps({
-        'avg_age': math.floor(sum([employee['age'] for employee in employees_data]) / len(employees_data)),
-        'avg_salary': math.floor(sum([int(e['salary'][1:]) for e in employees_data]) / len(employees_data)),
+        'avg_age': math.floor(sum([employee.age for employee in employees]) / len(employees)),
+        'avg_salary': math.floor(sum([employee.salary for employee in employees]) / len(employees)),
         'avg_yearly_increase': yearly_avg_increase,
-        'max_salary': max_salary,
-        'min_salary': min_salary,
+        'max_salary': [employee.to_dict() for employee in max_salary_employees],
+        'min_salary': [employee.to_dict() for employee in min_salary_employees],
     })
 
 
 def main():
-    print(stats(json.loads(f"[{EMPLOYEE_JSON}]"), starting_age=24, starting_salary=30000))
+    print(stats(f"[{EMPLOYEE_JSON}]", starting_age=24, starting_salary=30000))
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -50,7 +50,7 @@ class DataStatsTests(unittest.TestCase):
             ],
         }
         self.assertEqual(
-            json.loads(datastats.stats(json.loads(EMPLOYEES_JSON), iage=24, isalary=30000)),
+            json.loads(datastats.stats(EMPLOYEES_JSON, starting_age=24, starting_salary=30000)),
             expected)
 
 
